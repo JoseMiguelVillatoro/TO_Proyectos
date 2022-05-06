e4075a52945323996f35433098b5cb4f001da482
Jose Villatoro <mike.villatoro@hotmail.com>
2022-05-06 14:09:54 -0600

    Actualizando Datos 02:09 pm

--- app.js
+++ app.js
@@ -2,24 +2,13 @@ const express = require('express')
 const app = express()
 
 
-//Para traer el formuladio
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
 
 
 app.use(express.json())
 
 app.get('/', function (req, res) {
-    res.send('Hola Mundo! :D')
+    res.send('Hola Mundo! :D Prueba 2')
   })
 
-
-  app.post('/imagen', upload.single('imagen'), function(req,res){
-    const body = req.body
-    const imagen = req.file
-
-    console.log(imagen)
-    res.send('Hola Mundo desde el Post de Imagen')
-})
 
 app.listen(3000)
